1ef18351495a42b24b27b572323fe271d31900b8
liukang <pokaiqwe@gmail.com>
2024-10-24 17:06:42 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+
+Route::get('/', [CustomerController::class, 'index'])->name('home');
+
+
+Route::resource('customers', CustomerController::class);
